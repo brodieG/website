cc8a00869f4e5727470b083af340991b0fb22572
brodieG <brodieG@users.noreply.github.com>
2019-08-12 20:56:48 -0400

    cleanup, black background, fixes
    Shadow is now moved 1px so it grabs sides of tall bars.
    cubic interpolation testing.
    black background.
    removed junk

--- static/post/2019-07-26-hydra-loose-ends_files/scripts/threed-plot.R
+++ static/post/2019-07-26-hydra-loose-ends_files/scripts/threed-plot.R
@@ -6,18 +6,17 @@
 # * Seems a bit of a pita b/c we have to figure out nesting, etc.
 # For our simple case, much easier to just compute directly on the image.
 
-png <- png::readPNG('~/Downloads/colsums2/img-012.png') * 255
-RNGversion("3.5.2"); set.seed(42)
+# First find out what cells have gray colors
+
 set.seed(1)
 gn <- 10
 g2 <- sample(seq_len(gn), 95, replace=TRUE)
+x <- runif(length(g2))
 o <- order(g2)
 go <- g2[o]
-x <- runif(length(g2))
 xo <- x[o]
 
-# First find out what cells have gray colors
-
+png <- png::readPNG('~/Downloads/colsums2/img-012.png') * 255
 gray <- which(
   rowSums(
     abs(png[,,1:3] - c(rowSums(png[,,1:3], dims=2)) / 3), dims=2
@@ -97,73 +96,20 @@ elev.end <- make_elev(
   map.pix.x2, map.pix.y2, 1, 10, elev.end, c(rowsum(xo, go))
 )
 
-
-# With the tiles we can create an elevation map using the original values, there
-# are only 95 tiles so we're going to be lazy and do them in a for loop
-
-#
-# Use the elevation map to compute shade
-
-# shade <- shadow::ray_shade2(elev * , sunangle=315-90, anglebreaks=seq(30,60,1))
-
-shade_png <- function(png, root, elev, angles, deltas, delta.fac) {
-  png.orig <- png
-  for(i in seq_along(angles)) {
-    png <- png.orig
-    shade <- rayshader::ray_shade(
-      elev * 25, sunangle=-60, lambert=FALSE,
-      anglebreaks=angles[i] + (deltas * delta.fac[i]), maxsearch=300
-    ) * .7 + .3
-    png[,,1:3] <- png[,,1:3] * c(shade[,rev(seq_len(ncol(shade)))])
-    png::writePNG(png, sprintf(root, i))
-  }
-}
-angles <- seq(45, 90, by=5)
-deltas <- ((-5):5) / 10
-delta.fac <- seq(1, 0, length.out=length(deltas))
-
-shade_png(
-  png::readPNG('~/Downloads/colsums2/img-012.png'),
-   '~/Downloads/colsums2/rs-img-%03d.png',
-  elev.end, rev(angles), rev(deltas), rev(delta.fac)
-)
-shade_png(
-  png::readPNG('~/Downloads/colsums2/img-002.png'),
-   '~/Downloads/colsums2/as-img-%03d.png',
-  elev.start, angles, deltas, delta.fac
-)
-
-# remove color profile form existing files
-
-start <- 2;
-end <- 12;
-
-png.root <- '~/Downloads/colsums2/img-%03d.png'
-for(i in seq(start, end, by=1)) {
-  png.tmp <- png::readPNG(sprintf(png.root, i))
-  png::writePNG(png.tmp, sprintf(png.root, i))
-}
-
-png.test <- png::readPNG()
-png::writePNG(png.test, '~/Downloads/colsums2/img-012a.png')
-png.test2 <- png::readPNG('~/Downloads/colsums2/img-012a.png')
-
-par(mai=numeric(4))
-
-# downsample
-
-esmall <- elev.start[1:400,1:400]
-png.start <-  png::readPNG('~/Downloads/colsums2/img-002.png')
-psmall <- png.start[1:400, 1:400,]
-psmallbw <- rowMeans(psmall[,,1:3], dims=2)
-
-downsample <- function(mx) {
-  mx.tmp <- (mx[, rep(c(T,F), ncol(mx)/2)] + mx[, rep(c(F,T), ncol(mx)/2)]) / 2
-  (
-    mx.tmp[rep(c(T,F), nrow(mx.tmp)/2),] + 
-    mx.tmp[rep(c(F,T), nrow(mx.tmp)/2),]
-  ) / 2
-}
+# # downsample
+# 
+# esmall <- elev.start[1:400,1:400]
+# png.start <-  png::readPNG('~/Downloads/colsums2/img-002.png')
+# psmall <- png.start[1:400, 1:400,]
+# psmallbw <- rowMeans(psmall[,,1:3], dims=2)
+# 
+# downsample <- function(mx) {
+#   mx.tmp <- (mx[, rep(c(T,F), ncol(mx)/2)] + mx[, rep(c(F,T), ncol(mx)/2)]) / 2
+#   (
+#     mx.tmp[rep(c(T,F), nrow(mx.tmp)/2),] + 
+#     mx.tmp[rep(c(F,T), nrow(mx.tmp)/2),]
+#   ) / 2
+# }
 
 library(shadow)
 png.start <-  png::readPNG('~/Downloads/colsums2/img-002.png')
@@ -171,7 +117,8 @@ png.end <-  png::readPNG('~/Downloads/colsums2/img-012.png')
 png.start.bw <- rowMeans(png.start[,,1:3], dims=2)
 png.end.bw <- rowMeans(png.end[,,1:3], dims=2)
 render_frame <- function(
-  png, elev, r=diag(3), mult=20, d=Inf, anglebreaks=45, max.el=max(elev)
+  png, elev, r=diag(3), mult=20, d=Inf,
+  anglebreaks=45, max.el=max(elev), empty=1
 ) {
   if(length(dim(png)) == 2) dim(png) <- c(dim(png), 1L)
   shade <- rayshader::ray_shade(
@@ -186,11 +133,12 @@ render_frame <- function(
   shade <- shade[,rev(seq_len(ncol(shade)))]
   res <- vapply(seq_len(dim(png)[3]),
     function(x) {
-      elev[1L, dim(elev)[2]] <- max.el  # top right hand corner lifted
-      png[1L,dim(elev)[2],x] <- 0
+      # lift a corner so that on rotation resizing consistent
+      elev[1L, dim(elev)[2]] <- max.el         # top right hand corner lifted
+      png[1:2,dim(elev)[2]-(0:1),x] <- empty   # change this to see corner
       message("Rendering layer ", x)
       shadow::render_elevation_rel(
-        elev * mult, png[,,x] * shade, r, zord='pixel', d=d, empty=1
+        elev * mult, png[,,x] * shade, r, zord='pixel', d=d, empty=empty
       )
     },
     png.start[,,1]
@@ -199,7 +147,8 @@ render_frame <- function(
   res
 }
 render_frames <- function(
-  png, elev, rs, ds, angles, breaks, breaks.mult, max.el=max(elev)
+  png, elev, rs, ds, angles, breaks, breaks.mult, max.el=max(elev), empty=1,
+  mult=20
 ) {
   stopifnot(
     length(rs) == length(ds),
@@ -212,115 +161,66 @@ render_frames <- function(
       render_frame(
         png, elev, r=rs[[x]], d=ds[[x]],
         anglebreaks=breaks * breaks.mult[[x]] + angles[[x]],
-        max.el=max.el
+        max.el=max.el, empty=empty, mult=mult
       )
   )
 }
-frame.n <- 10
-d.start <- 1e4
-d.end <- 1e3
-ds <- c(Inf, seq(sqrt(d.start), 0, length.out=frame.n - 1)^2 + d.end)
+el.mult <- 20
+el.max.val <- el.mult * max(elev.end)
+frame.n <- 50
+
+seq.base <- 5
+seq.vals.3 <- seq(from=-(seq.base^3), to=seq.base^3, length.out=frame.n)
+sign <- sign(seq.vals.3)
+seq.vals <- round((sign * abs(seq.vals.3) ^ (1/3) + seq.base)/ (2*seq.base), 7)
+
+make_path <- function(min, max) {
+  rng <- c(min, max)
+  diff(rng) * seq.vals + min(rng)
+}
+# we want our base line distance from base of model to top of model to distance
+# to be more or less linear (not entirely correct as it doesn't account for
+# rotation)
+
+d.inv.start <- el.max.val / (1e3 - el.max.val)
+d.inv.end <- el.max.val / (1e5 - el.max.val)
+ds <- rev(el.max.val / make_path(d.inv.end, d.inv.start) + el.max.val)
+rs <- lapply(make_path(-20, 0), rot_y)
+angles <- make_path(45, 90)
+breaks <- seq((-2):2)
+breaks.mult <- make_path(1, 0)
+# png.root <- '~/Downloads/colsums2/z3d-img-%03d.png'
+png.root <- '~/Downloads/colsums-tests/z3d-img-%03d.png'
+
 frames.3d <- render_frames(
-  png.end[,,1:3], elev=elev.end,
-  rs=lapply(seq(0,-20,length.out=frame.n), rot_y),
-  ds=ds,
-  angles=seq(90, 45, length.out=frame.n),
-  breaks=seq((-2):2), breaks.mult=seq(0, 1, length.out=frame.n)
+  png.end[,,1:3], elev=elev.end, rs=rev(rs), ds=rev(ds), angles=rev(angles),
+  breaks=breaks, breaks.mult=rev(breaks.mult), empty=0, mult=el.mult
 )
-png.root <- '~/Downloads/colsums2/3d-img-%03d.png'
 for(i in seq_along(frames.3d)) {
   frame <- frames.3d[[i]]
   png::writePNG(frame, sprintf(png.root, i))
 }
 frames.3d2 <- render_frames(
-  png.start[,,1:3], elev=elev.start,
-  rs=lapply(seq(0,-20,length.out=frame.n), rot_y),
-  ds=ds,
-  angles=seq(90, 45, length.out=frame.n),
-  breaks=seq((-2):2), breaks.mult=seq(0, 1, length.out=frame.n),
-  max.el=max(elev.end)
+  png.start[,,1:3], elev=elev.start, rs=rs, ds=ds, angles=angles,
+  breaks=breaks, breaks.mult=breaks.mult, max.el=max(elev.end), empty=0
 )
+png.root <- '~/Downloads/colsums-tests/3d-start-img-%03d.png'
+for(i in seq_along(frames.3d2)) {
+  frame <- frames.3d2[[i]]
+  png::writePNG(frame, sprintf(png.root, i))
+}
 
-png3d <- render_frames(
-  png.end[,,1:3], elev.end, r=rot_y(-10), d=1000
+zz <- render_frame(
+  png.end[,,1:3], elev.end, r=rot_y(-20), anglebreaks=45, d=1e3, empty=0
 )
-par(mai=numeric(4))
-plot(as.raster(round(png3d*255)/255))
-
-el <- matrix(0, 100, 100)
-tx <- matrix(.8, 100, 100)
-
-xx <- shadow::render_elevation_rel(
-  el, tx, rot_x(0), zord='pixel', d=10, empty=0
+plot(as.raster(zz))
+zz <- render_frame(
+  png.end[,,1:3], elev.end, r=rot_y(0), anglebreaks=90, d=Inf, empty=0
 )
+plot(as.raster(round(zz*255)/255))
 
 
-png('~/Downloads/tmp.png', width=dim(pngbw)[2], height=dim(pngbw)[1])
-par(mai=numeric(4))
-plot(as.raster(xx))
-dev.off()
-
-flip <- function(x) t(x)[rev(seq_len(ncol(x))),]
-par(mai=numeric(4))
-plot(as.raster(xx))
-
-elevation <- exs * 50
-texture <- ps
-rotation <- rot_x(45) %*% rot_y(-45)
-rotation <- rot_y(-10)
-zord <- 'mesh'
-
-rl <- shadow:::rotate(
-  elevation = elevation, texture = texture, rotation = rotation
-)
-rlp <- persp_rel(rl, 1)
-resolution <- 400L
-rlps <- shadow:::scale_rel(rlp, resolution)
-mesh <- shadow:::mesh_tri(rlps, dim(elevation), order = FALSE)
-
-# ## Rescale data to a range from 0 to `range` where `range` in (0,1]
-# rescale <- function(x, range=1, center=0.5)
-#   ((x - min(x, na.rm=TRUE)) / diff(range(x, na.rm=TRUE))) * range +
-#    (1 - range) * center
-# 
-# ## Prepare a plot with a particular aspect ratio
-# plot_new <- function(
-#   x, y, xlim=c(0,1), ylim=c(0,1),
-#   par.args=list(mai=numeric(4L), xaxt='n', yaxt='n', xaxs='i', yaxs='i')
-# ) {
-#   if(length(par.args)) do.call(par, par.args)
-#   plot.new()
-#   plot.window(
-#     xlim, ylim, asp=diff(range(y, na.rm=TRUE))/diff(range(x, na.rm=TRUE))
-# ) }
-# 
-# mesh.tri <- mesh
-# x <- do.call(rbind, c(mesh.tri[,'x'], list(NA)))
-# y <- do.call(rbind, c(mesh.tri[,'y'], list(NA)))
-# col <- rep(c('#CCCCFF','#CCFFCC'), each=ncol(x)/2)
-# plot_new(x, y)
-# polygon(rescale(x), rescale(y), col=col)
-# plot_new(rlp$x,rlp$y)
-# points(rescale(rlp$x), rescale(rlp$y), col=gray(rlp$t), pch=16)
-
-res <- shadow:::rasterize(mesh, attr(rlps, "resolution"), 'pixel', 0)
-plot(as.raster(res))
-
-par(mai=numeric(4))
-plot(as.raster(res))
-
-
-mesh.tri <- mesh
-lim <- 120000
-off <- 0
-idx <- seq(lim * 4) + off * 4
-x <- do.call(rbind, c(mesh.tri[,'x'], list(NA)))
-y <- do.call(rbind, c(mesh.tri[,'y'], list(NA)))
-texture <- gray((Reduce('+', mesh.tri[,'t'])/nrow(mesh.tri)))
-plot_new(x, y)
-polygon(rescale(x), rescale(y), col=texture, border=texture)
-
-
-
-
-ggplot(tiles) + geom_rect(aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax))
+#ffmpeg -pattern_type glob -i '*.png' -r 30 -pix_fmt yuv420p out.mp4
+#ffmpeg -pattern_type glob -i '*.png' -vf "fps=5,format=yuv420p" out.mp4
+#ffmpeg -r 1/5 -i img%03d.png -c:v libx264 -vf "fps=25,format=yuv420p" out.mp4
+"
