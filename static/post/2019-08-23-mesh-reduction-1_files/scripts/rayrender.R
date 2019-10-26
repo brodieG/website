8df90ef222e8b94932d20576aa9aee2f8d1cf932
brodieG <brodieG@users.noreply.github.com>
2019-10-26 08:31:31 -0400

    making cubes

--- static/post/2019-08-23-mesh-reduction-1_files/scripts/rayrender.R
+++ static/post/2019-08-23-mesh-reduction-1_files/scripts/rayrender.R
@@ -6,6 +6,7 @@ x.width <- 1
 x.off <- seq(-x.width / 2, x.width/2, length.out=length(x))
 
 floor.mult <- 1.25
+wall.mult <- floor.mult/3*2
 scene <- xz_rect(
   xwidth=floor.mult*x.width, 
   zwidth=floor.mult*x.width,
@@ -15,12 +16,12 @@ scene <- xz_rect(
   material=lambertian(color='grey35')
 )
 scene <- add_object(scene,
-  xy_rect(
-    xwidth=floor.mult*x.width, ywidth=floor.mult*x.width,
-    z=floor.mult*x.width/2, y=floor.mult*x.width/2,
+  yz_rect(
+    zwidth=floor.mult*x.width, ywidth=wall.mult*x.width,
+    z=floor.mult*x.width/2, y=wall.mult*x.width/2,
     # material=lambertian(checkercolor='black', checkerperiod=x.width/length(x)),
-    material=lambertian(color='grey35'),
-    flipped=TRUE
+    material=lambertian(image=image),
+    angle=c(0, 90, 0), #flipped=TRUE
 ) )
 # scene <- generate_ground(material = lambertian())
 for(i in seq_along(x)) {
@@ -45,25 +46,89 @@ final <- add_object(
     material = lambertian(lightintensity = 3000, implicit_sample = TRUE)
 ) )
 render_scene(
-  final, parallel = TRUE, width = 200, height = 200, samples = 1000,
-  lookfrom=c(0,1,-2), lookat=c(0,0,1), fov=30,
+  final, parallel = TRUE, 
+  width = 200, height = 200, samples = 200,
+  # width = 600, height = 600, samples = 1000,
+  lookfrom=c(0,1,-1), lookat=c(0,0,1), fov=45,
   ambient_light=FALSE, aperture=.0, clamp=5
 )
 
-generate_cornell() %>%
-  add_object(
-    xz_rect(
-      x = 555/2, y = 0, z = 555/2, xwidth = 200, zwidth = 200,
-      material = lambertian())
-   ) %>%
-   render_scene(lookfrom = c(278, 278, -800) ,lookat = c(278, 278, 0), fov = 40, 
-   ambient_light = FALSE, samples = 400, parallel = TRUE, clamp_value = 5, width=200, height=200)
+library(watcher)
+library(gganimate)
+library(ggplot2)
 
-scene <- add_object(
-  scene,
-  cube(
-    x=0, z=.05, y=.2, ywidth=.4, xwidth=.1, zwidth=.1,
-    angle=c(0, 22.5, 0),
-    material=dielectric(color='red')
-) )
+# - Text stuff
 
+set.seed(1220)
+x <- runif(10)
+raw <- watch(insert_sort, c('i', 'j', 'x'))(x)
+dat <- simplify_data(attr(raw, 'watch.data'))
+code.txt <- expand_text(attr(raw, 'watch.code'), dat)
+
+dpi <- 72
+width <- 800
+height <- width/3*2
+dev.off()
+dev.new(width=width/dpi, height=width/dpi, dpi=dpi)
+
+p <- ggplot(subset(code.txt, .id==1)) +
+  geom_text(
+    aes(x=0, y=y.raw, label=code),
+    hjust=0, size=12, family='mono'
+  ) + 
+  labs(y=NULL, x=NULL) +
+  theme(
+    axis.text.x=element_blank(), axis.text.y=element_blank(),
+    axis.ticks.x=element_blank(), axis.ticks.y=element_blank(),
+    panel.grid=element_blank(),
+    panel.background = element_rect(fill = "transparent", colour = NA),
+    plot.background = element_rect(fill = "transparent", colour = NA),
+    plot.margin=unit(c(0, 0, height/3/dpi, 0), "inches"),
+  ) +
+  coord_cartesian(ylim=c(0,12), xlim=c(0, 60)) +
+  NULL
+
+
+i <- 1
+ggsave(
+  filename=sprintf('~/Downloads/ray-anim/img-%04d.png', i),
+  plot=p,
+  width=width/dpi, height=height/dpi, units='in', device='png',
+  dpi=dpi, bg='grey35'
+)
+image <- png::readPNG(sprintf('~/Downloads/ray-anim/img-%04d.png', i))
+
+
+## Takes a set of coordinates and makes cubes centered on each of those
+## coordinates.  Currently assumes coord are x/y and z == 0. Objects are
+## centered on zero in x and scaled such that range(y) == c(0,1)
+
+as_cubes <- function(
+  vals, material=lambertian(),
+  angle=c(0,0,0), order_rotation=1:3, scale=c(1,1,1)
+) {
+  x.rng <- diff(range(vals[,1]))
+  y.rng <- diff(range(vals[,1]))
+  scale.fac <- 1 / (y.rng + 1)
+  vals[,2] <- -vals[,2]
+  vals[,1] <- max(vals[,1]) - vals[,1] + 1/2
+  vals[,2] <- max(vals[,2]) - vals[,2] - y.rng / 2
+  vals <- vals * scale.fac
+
+  obj <- NULL
+  for(i in 1:nrow(vals)) {
+    obj <- add_object(
+      obj, cube(
+        x=vals[i,2], y=vals[i,1], material=material, width=scale.fac
+  ) ) }
+  group_objects(
+    obj, group_angle=angle, group_order_rotation=order_rotation, 
+    group_scale=scale
+  )
+}
+i.vals <- which(!read.csv('~/Downloads/i.csv'), arr.ind=TRUE)
+i.obj <- as_cubes(i.vals, material=lambertian(color='red'))
+j.vals <- which(!read.csv('~/Downloads/j.csv'), arr.ind=TRUE)
+j.obj <- as_cubes(i.vals, material=lambertian(color='red'))
+
+render_scene(i.obj)
