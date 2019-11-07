0810700b96b4f0d278b4657d833eed60b91030b4
brodieG <brodieG@users.noreply.github.com>
2019-11-06 20:16:29 -0500

    batch script

---
+++ static/post/2019-08-23-mesh-reduction-1_files/scripts/viz-batch.R
@@ -0,0 +1,166 @@
+
+width <- 200
+samples <- 200
+
+# - stacked meshes -------------------------------------------------------------
+
+make_layers <- function(
+  errors, tol, colors, radius, shallowness, offset
+) {
+  z.off <- 0.5
+  colors <- rep_len(colors, length(tol))
+
+  meshes <- lapply(tol, extract_mesh2, errors=errors)
+  segs <- lapply(
+    seq_along(meshes), function(i) {
+      writeLines(sprintf('running %d', i))
+      mat <- metal(color=colors[i])
+      tris_to_seg(meshes[[i]], map, radius=radius, material=mat, flatten=TRUE)
+    }
+  )
+  diag.off <- (seq_along(tol) - ceiling(length(tol)/2)) * offset
+  layers <- lapply(
+    seq_along(segs),
+    function(i) {
+      group_objects(
+        segs[[i]],
+        group_angle=c(90, 90, 0),
+        group_translate=c(
+          +0.5 + diag.off[i], i/(length(segs)*shallowness), zoff - diag.off[i]
+        ),
+        pivot_point=numeric(3)
+    ) }
+  )
+}
+
+# offset 3 stack
+
+ err.frac <- elmax/c(30,15,3)
+ mesh.colors <- c('gold', 'grey65', '#DD4F12')
+ light.wide <- sphere(
+   y=8, z = 0, x = 0, radius = 3,
+   material = lambertian(lightintensity = 7, implicit_sample = TRUE)
+ )
+ floor <- xz_rect(xwidth=5, zwidth=5, material=lambertian(color='white'))
+ objs <- make_layers(errors, err.frac, mesh.colors, radius=seg.rad,  10, seg.rad*2)
+ scene <- dplyr::bind_rows(objs, list(light.wide, floor))
+ 
+ render_scene(
+   scene, width=width, height=width, samples=samples,
+   lookfrom=c(0, 2, 0), lookat=c(0, 0, 0), aperture=0,
+   fov=0, ortho_dimensions=c(1.25,1.25), camera_up=c(1,0,0),
+   clamp=3, file='~/Downloads/mesh-viz/batch-1.png'
+ )
+ # full 8 stack
+ 
+ err.frac <- rev(elmax/2^(0:7))
+ mesh.colors <- 'gold'
+ light.wide <- sphere(
+   y=8, z = 0, x = 0, radius = 3,
+   material = lambertian(lightintensity = 7, implicit_sample = TRUE)
+ )
+ floor <- xz_rect(xwidth=5, zwidth=5, material=lambertian(color='white'))
+ objs <- make_layers(errors, err.frac, mesh.colors, radius=seg.rad, 3, 0)
+ scene <- dplyr::bind_rows(objs, list(light.wide, floor))
+ 
+ render_scene(
+   scene, width=width, height=width, samples=samples,
+   lookfrom=c(0, 2, 0), lookat=c(0, 0, 0), aperture=0, fov=34.5,
+   camera_up=c(1,0,0),
+   clamp=3, file='~/Downloads/mesh-viz/batch-2.png'
+ )
+ # viridis 8 stack
+ 
+ err.frac <- rev(elmax/2^(0:7))
+ mesh.colors <- substr(viridisLite::viridis(8), 1, 7)
+ light.wide <- sphere(
+   y=8, z = 0, x = 0, radius = 3,
+   material = lambertian(lightintensity = 7, implicit_sample = TRUE)
+ )
+ floor <- xz_rect(xwidth=5, zwidth=5, material=lambertian(color='white'))
+ objs <- make_layers(errors, err.frac, mesh.colors, radius=seg.rad, 3, 0)
+ scene <- dplyr::bind_rows(objs, list(light.wide, floor))
+ 
+ render_scene(
+   scene, width=width, height=width, samples=samples,
+   lookfrom=c(0, 2, 0), lookat=c(0, 0, 0), aperture=0, fov=34.5,
+   camera_up=c(1,0,0),
+   clamp=3, file='~/Downloads/mesh-viz/batch-3.png'
+ )
+# abreast top
+
+zoff <- +.5
+mesh.colors <- c('gold', 'grey25', '#DD4F12')
+mat0 <- metal(color=mesh.colors[1])
+mat2 <- metal(color=mesh.colors[2])
+mat3 <- metal(color=mesh.colors[3])
+seg0f <- tris_to_seg(tris0, map, radius=seg.rad, material=mat0, flatten=TRUE)
+seg2f <- tris_to_seg(tris2, map, radius=seg.rad, material=mat2, flatten=TRUE)
+seg3f <- tris_to_seg(tris3, map, radius=seg.rad, material=mat3, flatten=TRUE)
+
+light.narrow <- sphere(
+  y=8, z = 4, x = 0, radius = .2,
+  material = lambertian(lightintensity = 2000, implicit_sample = TRUE)
+)
+scn <- add_object(
+  light.wide,
+  group_objects(
+    seg0f, group_angle=c(90, 90, 0), group_translate=c(-.75, 0, zoff),
+    pivot_point=numeric(3)
+) )
+scn <- add_object(
+  scn,
+  group_objects(
+    seg2f,
+    group_angle=c(90, 90, 0), group_translate=c(+0.5, 0, zoff),
+    pivot_point=numeric(3)
+) )
+scn <- add_object(
+  scn,
+  group_objects(
+    seg3f,
+    group_angle=c(90, 90, 0), group_translate=c(+1.75, 0, zoff),
+    pivot_point=numeric(3)
+) )
+scn <- add_object(scn, floor)
+render_scene(
+  scn, width=width/400*150, height=width, samples=samples,
+  lookfrom=c(0, sqrt(sum(c(4, 2)^2)), 0), lookat=c(0, 0, 0), aperture=0, fov=0,
+  ortho_dimensions=c(1.5,4), camera_up=c(1,0,0),
+  clamp=3, file='~/Downloads/mesh-viz/batch-4.png'
+)
+# abreast side
+
+seg0s <- tris_to_seg(tris0, map, radius=seg.rad, material=mat0)
+seg2s <- tris_to_seg(tris2, map, radius=seg.rad, material=mat2)
+seg3s <- tris_to_seg(tris3, map, radius=seg.rad, material=mat3)
+
+scn <- add_object(
+  light.narrow,
+  group_objects(
+    seg0s, group_angle=c(90, 90, 0), group_translate=c(-.75, 0, zoff),
+    pivot_point=numeric(3)
+) )
+scn <- add_object(
+  scn,
+  group_objects(
+    seg2s,
+    group_angle=c(90, 90, 0), group_translate=c(+0.5, 0, zoff),
+    pivot_point=numeric(3)
+) )
+scn <- add_object(
+  scn,
+  group_objects(
+    seg3s,
+    group_angle=c(90, 90, 0), group_translate=c(+1.75, 0, zoff),
+    pivot_point=numeric(3)
+) )
+scn <- add_object(
+  scn, xz_rect(xwidth=5, zwidth=5, material=lambertian(color='white'))
+)
+render_scene(
+  scn, width=width, height=width/400*150, samples=samples,
+  lookfrom=c(0, 4, 2), lookat=c(0, 0, 0), aperture=0, fov=0,
+  ortho_dimensions=c(4,1.5),
+  clamp=3, file='~/Downloads/mesh-viz/batch-5.png'
+)
