a4c6984b8dd398b9bd6112b404ba1388bd582a7d
brodieG <brodieG@users.noreply.github.com>
2019-12-15 21:58:28 -0500

    add cmp_err2a, and move extract to new file

--- static/script/mesh-viz/rtin-vec.R
+++ static/script/mesh-viz/rtin-vec.R
@@ -210,289 +210,85 @@ compute_error2 <- function(map) {
   }
   errors
 }
-# 1. start from the lowest level out
-# 2. check diag point error, if fail:
-# 3. get child diag coordinates
-# 4. if NA, already plotted, don't plot
-# 5. else, generate child triangles
-# 6. mark diag point as NA
-# 7. at top level, draw remaining children
+# Works for non-square grids (when we're done with it)
 
-# Alternative (more like original)
-#
-# 1. Start from top level
-#    * Get initial ids, in theory spreading out from those should guarantee
-#    getting all the midpoints.
-# 2. If not failing, draw the two triangles
-#    * need to know if vert/hz tr/tl
-# 3. If failing, get children
-# 4. repeat
+compute_error2a <- function(map) {
+  # - Helper Funs --------------------------------------------------------------
+  .get_child_err <- function(ids.mid, which, type) {
+    if(identical(type, 'axis')) {          # square sides
+      col.off <- c(-1L, 1L, 1L, -1L)
+      row.off <- c(-1L, -1L, 1L, 1L)
+    } else if (identical(type, 'diag')) {  # diagonals
+      col.off <- c(0L, 2L, 0L, -2L)
+      row.off <- c(-2L, 0L, 2L, 0L)
+    } else stop("bad input")
+    offset <- (row.off * mult) %/% 4L + nr * (col.off * mult) %/% 4L
+    lapply(
+      seq_along(offset)[which], function(i) errors[ids.mid + offset[i]]
+  ) }
+  .get_par_err <- function(ids, offsets)
+    abs(map[ids] - (map[ids + offsets[1L]] + map[ids + offsets[2L]]) / 2)
 
-# Triangle Base Coords
-#
-# Returns for each id, the four pairs of ids of the base vertices connected
-# to that ids.  E.g, for `ids.mid` x, will return a,b,c,d, with first the
-# two ids for a, then b, etc.
-#
-#              a---b                  a
-#              |\ /|                 /|\
-#  type = 'd': | x |    type = 's': d-x-b
-#              |/ \|                 \|/
-#              c---d                  c
-
-base_coords <- function(ids.mid, type, nr, nc, mult) {
-  if(identical(type, 's')) {
-    r.off <- c(+0L, +1L, +1L, +0L, +0L, -1L, -1L, +0L)
-    c.off <- c(+1L, +0L, +0L, -1L, -1L, +0L, +0L, +1L)
-  } else if(identical(type, 'd')) {
-    r.off <- c(-1L, +1L, +1L, +1L, +1L, -1L, -1L, -1L)
-    c.off <- c(+1L, +1L, +1L, -1L, -1L, -1L, -1L, +1L)
-  }
-  off <- r.off * mult %/%2L + c.off * (nr * mult %/%2L)
-  rep(ids.mid, each=length(off)) + rep(off, length(ids.mid))
-}
-# Alternate extract mesh algo:
-#
-# 1. Compute long edge midpoints at lowest level
-# 2. If error > tolerance:
-# 3.   Find the four triangles that have that midpoint for a vertex
-# 4.   Check which of those have undrawn vertices
-# 5.   Draw them
-# 6.   Mark drawn vertices as drawn
-# 7. Increase level
-# 8. Compute long edge midpoints
-# 9. Go to 2.
-
-# problem right now is that when we add the fake errors to cause the partial
-# strips to be drawn our logic only works if we have adjscent size strips
-# happening, if we skip one size then there is no conveyance of the break, the
-# break needs to carry all the way.
-
-# @param debug.lvl extract all triangles at that level, and only that level
-#   (pass along a zero error matrix), ignored if zero
-
-extract_mesh2 <- function(errors, tol, debug.lvl=0) {
-  stopifnot(length(errors) > 1L)
-  nr <- nrow(errors)
-  nc <- ncol(errors)
-  layers <- floor(min(log2(c(nr, nc))))
-  undrawn <- array(TRUE, dim(errors))
-  triangles <- vector('list', 2L * layers)
+  nr <- nrow(map)
+  nc <- ncol(map)
+  layers <- floor(min(log2(c(nr, nc) - 1L)))
+  errors <- array(0, dim=dim(map))
 
   for(i in seq_len(layers)) {
     mult <- as.integer(2^i)
-    grid.nr <- ((nr - 1L) %/% mult)
-    grid.nc <- ((nc - 1L) %/% mult)
-    nr.g <- grid.nr * mult + 1L
-    nc.g <- grid.nc * mult + 1L
+    mhalf <- mult %/% 2L
+    tile.r <- ((nr - 1L) %/% mult)
+    tile.c <- ((nc - 1L) %/% mult)
 
-    # - Vertical and Horizontal Bases ("diamond?") -
+    # - Axis (vertical/horizontal) ---------------------------------------------
 
-    # Index munging to compute midpoints for long edges that are vertical
-    # or horizontal
-
-    c.lens <- rep_len(c(grid.nr, grid.nr + 1L), grid.nc * 2L + 1L)
-    ids.len <- sum(c.lens)
-    ids.r.raw <- rep_len(
-      c(seq(mult %/% 2L + 1L, nr.g, by=mult), seq(1L, nr.g, by=mult)),
-      length.out=ids.len
+    # Compute IDs, distinguish b/w inside vs. outside
+    col.vn <- tile.c + 1L
+    col.hn <- tile.c
+    col.v <- seq(1L, length.out=col.vn, by=mult)
+    col.h <- seq(2L, length.out=tile.c, by=mult)
+    ids.v <- seq(mhalf + 1L, nr, mult)
+    ids.h <- seq(nr * mhalf + 1L, nr * (mhalf + 1L), by=mult)
+    v.len <- length(ids.v)
+    h.len <- length(ids.h)
+    ids <- list(
+      v.out.l=ids.v,
+      v.in=ids.v + rep_each((col.v[-c(1L,col.vn)] - 1L) * nr, v.len),
+      v.out.r=ids.v + (col.v[col.vn] - 1L) * nr,
+      h.out.t=ids.h[1L] + (seq_len(col.hn) - 1L) * nr * mult,
+      h.in=ids.h[-c(1L, h.len)] +
+        rep_each((seq_len(col.hn)-1L) * nr * mult, h.len - 2L),
+      h.out.b=ids.h[h.len] + (seq_len(col.hn) - 1L) * nr * mult
     )
-    ids.c.raw <- (rep(seq_len(grid.nc * 2L + 1L), c.lens) - 1L) *
-      mult %/% 2L * nr
-    ids <- ids.c.raw + ids.r.raw
+    # Errors
+    err.child <- if(i > 1L) {
+      which <- list(2:3, 1:4, c(1L,4L), 3:4, 1:4, 1:2)
+      Map(.get_child_err, ids, which, 'axis')
+    }
+    err.par <- c(
+      Map(.get_par_err, ids[1:3], list(c(-mhalf, mhalf))),
+      Map(.get_par_err, ids[4:6], list(c(-mhalf * nr, mhalf * nr)))
+    )
+    for(j in seq_along(ids)) {
+      errors[ids[[j]]] <- do.call(
+        pmax, c(list(na.rm=TRUE), err.par[j], err.child[[j]])
+    ) }
+    # - Diagonals --------------------------------------------------------------
 
-    # Midpoints at edges of tile will produce oob triangles with our naive
-    # offset algorithm.  To detect the oob triangles we must add padding to our
-    # indices as otherwise oob offsets would overlap with valid offsets.
+    ids.raw <- seq(mhalf + nr * mhalf + 1L, nr * (mhalf + 1L), by=mult) +
+      matrix((seq_len(tile.c) - 1L) * mult * nr, tile.r, tile.c, byrow=TRUE)
+    which.sw <- xor((col(ids.raw) %% 2L), (row(ids.raw) %% 2L))
+    ids <- list(nw=ids.raw[!which.sw], sw=ids.raw[which.sw])
 
-    ids.pad <- ids + ids.c.raw + nr
-
-    # Error, or at edge of plot and we will see no more larger triangles when
-    # plot is strictly square and of 2^k + 1.
-
-    ids.err <- errors[ids] > tol | (i  * 2 - 1)== debug.lvl
-    ids.pad.err <- ids.pad[ids.err]
-
-    # Coords for the bases for each of four triangles that form the tile around
-    # each midpoint.
-
-    base.vert <- base_coords(ids.pad.err, type='s', nr * 2L, nc, mult)
-
-    # detect oob, and then undo padding
-
-    base.vert.inb <- base.vert > 0L & base.vert <= (nr * nc * 2L) &
-      (((base.vert - 1L) %/% nr) %% 2L) == 1L
-    base.vert <- base.vert - ((base.vert - 1L) %/% (nr * 2L) + 1L) * nr
-
-    # detect whether a triangle has already been drawn
-
-    tri.inb <- .colSums(base.vert.inb, m=2L, n=length(base.vert.inb)/2L) == 2L
-    base.vert.undrawn <- rep(TRUE, length(base.vert)/2L)
-    base.vert.mid.id <-
-      .colMeans(base.vert[rep(tri.inb, each=2L)], m=2L, n=sum(tri.inb))
-    base.vert.undrawn[tri.inb] <- undrawn[base.vert.mid.id]
-
-    # valid triangles are those that have no oob vertices and have at least one
-    # of two vertices not drawn.
-
-    tri.draw <- base.vert.undrawn & tri.inb
-
-    # generate triangle coords, and mark the vertices as drawn.
-
-    tri.draw.vert <- matrix(base.vert[rep(tri.draw, each=2L)], nrow=2)
-    triangles[[(i - 1L) * 2L + 1L]] <-
-      rbind(tri.draw.vert, rep(ids[ids.err], each=4L)[tri.draw])
-    undrawn[ids[ids.err]] <- FALSE
-
-    # - Diagonal Bases -
-
-    ids.r.raw <- seq(mult %/% 2L + 1L, length.out=grid.nr, by=mult)
-    ids.c.raw <- seq(nr * mult %/% 2L, length.out=grid.nc, by=nr * mult)
-    ids <- rep(ids.r.raw, each=length(ids.c.raw)) +
-      rep(ids.c.raw, length(ids.r.raw))
-
-    ids.err <- errors[ids] > tol |  i * 2L == debug.lvl
-    base.vert <- base_coords(ids[ids.err], type='d', nr, nc, mult)
-    base.vert.mid.id <- .colMeans(base.vert, m=2L, n=length(base.vert)/2L)
-    base.vert.undrawn <- undrawn[base.vert.mid.id]
-    tri.draw <- base.vert.undrawn
-
-    tri.draw.vert <- matrix(base.vert[rep(tri.draw, each=2L)], nrow=2)
-    triangles[[i * 2L]] <-
-      rbind(tri.draw.vert, rep(ids[ids.err], each=4L)[tri.draw])
-    undrawn[ids[ids.err]] <- FALSE
-
-    # Need to draw any undrawn triangles at this particular level (or maybe
-    # pair of levels?)
+    err.child <- Map(.get_child_err, ids, list(1:4), 'diag')
+    err.par <- Map(
+      .get_par_err, ids,
+      list((mhalf * nr + mhalf) * c(1L, -1L), (mhalf * nr - mhalf) * c(1L, -1L))
+    )
+    for(j in seq_along(ids)) {
+      errors[ids[[j]]] <- do.call(
+        pmax, c(list(na.rm=TRUE), err.par[j], err.child[[j]])
+    ) }
   }
-  triangles
+  errors
 }
-
-
-# shapes
-
-# for each shape
-# x/y offsets for each of the four child triangles, including NAs for missing
-# ordered starting from breakpoint (hypotenuse midpoint) clockwise.  Shapes are
-# in this order:
-#
-# 1. v.left
-# 2. v.mid
-# 3. v.right
-# 4. h.top
-# 5. h.mid
-# 6. h.bot
-# 7. tr->bl
-# 8. tl->br
-
-off.ex.tri <- array(
-  c(
-    0L,0L,   0L,1L,   1L,0L,    0L,0L,  1L,0L, 0L,-1L,  # vert left
-    NA,NA,   NA,NA,   NA,NA,    NA,NA,  NA,NA, NA,NA,
-
-    0L,0L,   0L,1L,   1L,0L,    0L,0L,  1L,0L, 0L,-1L,  # vert mid
-    0L,0L,  0L,-1L,  -1L,0L,    0L,0L, -1L,0L, 0L, 1L,
-
-    0L,0L,  0L,-1L,  -1L,0L,    0L,0L, -1L,0L, 0L,1L,   # vert right
-    NA,NA,   NA,NA,   NA,NA,    NA,NA,  NA,NA, NA,NA,
-
-    0L,0L,   1L,0L,  -1L,0L,    0L,0L, -1L,0L, -1L,0L,  # hrz top
-    NA,NA,   NA,NA,   NA,NA,    NA,NA,  NA,NA, NA,NA,
-
-    0L,0L,   1L,0L,  -1L,0L,    0L,0L, -1L,0L, -1L,0L,  # hrz mid
-    0L,0L,  -1L,0L,   0L,1L,    0L,0L,  0L,1L,  1L,0L,
-
-    0L,0L,  -1L,0L,   0L,1L,    0L,0L,  0L,1L,  1L,0L,  # hrz bot
-    NA,NA,   NA,NA,   NA,NA,    NA,NA,  NA,NA,  NA,NA,
-
-    0L,0L,  -1L,1L,   1L,1L,    0L,0L,   1L,1L, 1L,-1L,  # diag
-    0L,0L,  1L,-1L, -1L,-1L,    0L,0L, -1L,-1L, -1L,1L,
-
-    0L,0L,  -1L,1L,   1L,1L,    0L,0L,   1L,1L, 1L,-1L,  # diag (same)
-    0L,0L,  1L,-1L, -1L,-1L,    0L,0L, -1L,-1L, -1L,1L
-  ),
-  dim=c(2L, 3L, 8L)
-)
-# Offsets to get children from parent hypotenuse midpoint
-#
-# Alternate x/y coords, permuted so that after collapsing x/y into linear ID we
-# can select any of the eight types of offsets with the row selector
-
-off.ex.mid <- aperm(
-  array(
-    c(
-      1L,1L,   1L,-1L,  NA,NA,   NA,NA,    # vert left
-      1L,1L,   1L,-1L, -1L,1L,  -1L,-1L,   # vert mid
-      -1L,1L, -1L,-1L,  NA,NA,   NA,NA,    # vert right
-      -1L,1L,  1L,1L,   NA,NA,   NA,NA,    # hrz top
-       1L,1L,  1L,-1L, -1L,1L,  -1L,-1L,   # hrz mid
-      1L,-1L, -1L,-1L,  NA,NA,   NA,NA,    # hrz bot
-      0L,1L,   1L,0L,   0L,-1L, -1L,0L,    # diag
-      0L,1L,   1L,0L,   0L,-1L, -1L,0L     # diag
-    ),
-    dim=c(2L, 4L, 8L),
-  ),
-  c(1L, 3L, 2L)
-)
-
-# Maybe we really want to keep coordinates in x/y as we're going to expand
-# back to x/y here
-
-extract_tris <- function(id, nr) {
-  dx <- id[['x','tar']] - id[['x','par']]
-  dy <- id[['y','tar']] - id[['y','par']]
-
-  res.x <- rbind(id[['x','par']], id[['x','tar']] + dy, id[['x','tar']] - dy)
-  res.y <- rbind(id[['y','par']], id[['y','tar']] - dx, id[['y','tar']] + dx)
-
-  res.x * nr + res.y + 1L
-}
-next_children <- function(id) {
-  dx <- (id[['x','tar']] - id[['x','par']])/2L
-  dy <- (id[['y','tar']] - id[['y','par']])/2L
-  dx_p_dy <- dx + dy
-  dx_m_dy <- dx - dy
-
-  id[,'par'] <- lapply(id[,'tar'], rep, 2L)
-  id[,'tar'] <- list(
-      c(id[['x','tar']] - dx_p_dy, id[['x','tar']] - dx_m_dy),
-      c(id[['y','tar']] + dx_m_dy, id[['y','tar']] - dx_p_dy)
-  )
-  id
-}
-extract_mesh3 <- function(errors, tol) {
-  nr <- nrow(errors)
-  nc <- ncol(errors)
-  layers <- floor(min(log2(c(nr, nc) - 1L)))
-  tilesq <- as.integer((2L^(layers) + 1L) ^ 2)
-  id.dat <- id.pass <- id.fail <- matrix(
-    list(
-      rep(c(nc - 1L) %/% 2L, 2L), rep(c(nr - 1L) %/% 2L, 2L),
-      c(0L, nc - 1L), c(nr - 1L, 0L)
-    ),
-    2L, 2L, dimnames=list(c('x', 'y'), c('tar', 'par'))
-  )
-  res <- vector('list', 2L * layers + 1L)
-  for(i in seq_len(layers)) {
-    mult <- as.integer(2^(layers - i))
-
-    # diag first, then axis
-    for(j in 1:2) {
-      ids <- id.dat[['x','tar']] * nr + id.dat[['y','tar']] + 1L
-
-      pass <- errors[ids] <= tol
-      wpass <- which(pass)
-      wfail <- which(!pass)
-      id.pass[] <- lapply(id.dat, '[', wpass)
-      id.fail[] <- lapply(id.dat, '[', wfail)
-
-      res[[i * 2L - (j == 1L)]] <- extract_tris(id.pass, nr)
-
-      id.dat <- next_children(id.fail)
-  } }
-  # Any remaining failures must be split to lowest level
-
-  res[[2L * layers + 1L]] <- extract_tris(id.dat, nr)
-  res
-}
-
