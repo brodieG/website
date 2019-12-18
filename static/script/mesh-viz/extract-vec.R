864bb697f972b0bcebb975cd75602883bca5293d
brodieG <brodieG@users.noreply.github.com>
2019-12-17 21:19:19 -0500

    correct zig zag for ex_mesh3a

--- static/script/mesh-viz/extract-vec.R
+++ static/script/mesh-viz/extract-vec.R
@@ -306,7 +306,7 @@ seed_ids <- function(offset, m, length, hrz) {
   a <- rep(mids, 2L)
   b <- rep(m + offset, 2L * length)
   c <- c(mids - m, mids + m)
-  d <- b + rep(c(-m, m, m, -m), length.out=length * 2L)
+  d <- b + rep(c(m, -m, -m, m), length.out=length * 2L)
 
   matrix(
     if(hrz) list(a, b, c, d) else list(b, a, d, c),
