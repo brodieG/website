35a1019e1490c5c744af447afe795044af0e0609
brodieG <brodieG@users.noreply.github.com>
2019-12-20 13:45:39 -0500

    remove old offsets

--- static/script/mesh-viz/rtin-vec2.R
+++ static/script/mesh-viz/rtin-vec2.R
@@ -13,22 +13,6 @@
 offset.dg <- aperm(
   array(
     c(
-      1L,1L, 0L,0L, 2L,2L, 1L,2L, 0L,1L,
-      1L,1L, 0L,0L, 2L,2L, 1L,0L, 2L,1L,
-      3L,1L, 4L,0L, 2L,2L, 3L,2L, 4L,1L,
-      3L,1L, 4L,0L, 2L,2L, 2L,1L, 3L,0L,
-      1L,3L, 2L,2L, 0L,4L, 1L,4L, 2L,3L,
-      1L,3L, 2L,2L, 0L,4L, 0L,3L, 1L,2L,
-      3L,3L, 2L,2L, 4L,4L, 3L,4L, 2L,3L,
-      3L,3L, 2L,2L, 4L,4L, 3L,2L, 4L,3L
-    ),
-    dim=c(2L, 5L, 8L)
-  ),
-  c(3L, 1L, 2L)
-)
-offset.dg <- aperm(
-  array(
-    c(
       1L,1L, 0L,0L, 2L,2L, 1L,2L, 0L,1L, 1L,0L, 2L,1L,
       3L,1L, 4L,0L, 2L,2L, 3L,2L, 4L,1L, 2L,1L, 3L,0L,
       1L,3L, 2L,2L, 0L,4L, 1L,4L, 2L,3L, 0L,3L, 1L,2L,
