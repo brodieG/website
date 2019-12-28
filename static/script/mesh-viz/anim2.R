6c19988e5e0f0e196f4c1035fb153f5987cd03c2
brodieG <brodieG@users.noreply.github.com>
2019-12-28 12:06:32 -0800

    point type coloring

--- static/script/mesh-viz/anim2.R
+++ static/script/mesh-viz/anim2.R
@@ -4,17 +4,18 @@ source('static/script/mesh-viz/extract-vec.R')
 source('static/script/mesh-viz/viz-lib.R')
 
 library(watcher)
-vars <- c('o', 'o.m', 'o.a', 'o.b',  'o.len', 'errors', 'err.ids')
+library(reshape2)
+vars <- c(
+  'o', 'o.m', 'o.a', 'o.b',  'o.dim', 'errors', 'err.ids', 'c.rep','r.rep',
+  'j'
+)
 errors_watched <- watch(compute_error3b, vars)
 m <- map[1:5, 1:5]
 xx <- errors_watched(m)
 zz.raw <- simplify_data(attr(xx, 'watch.data'))
 
-library(watcher)
-library(reshape2)
 id.vars <- c('.id', '.line')
 zz.vec <- zz.raw[['.scalar']]
-dat <- melt(as.data.frame(zz.vec[c(vars, id.vars)]), id.vars=id.vars)
 
 # - Code -----------------------------------------------------------------------
 
@@ -59,31 +60,55 @@ for(i in seq_along(zz.vec$.id)) {
 # 
 # Urgh, we're probably not going to do this with the faster version.
 
+thm.blnk <- list(
+  theme(
+    axis.text.x=element_blank(), axis.text.y=element_blank(),
+    axis.ticks.x=element_blank(), axis.ticks.y=element_blank(),
+    panel.grid=element_blank()
+  ),
+  ylab(NULL),
+  xlab(NULL)
+)
+
 dpi <- 72
 width <- 600
-height <- width
+height <- width / 2
 size <- nrow(map)
 
 errors <- transform(
-  zz.raw$errors, x=(x - 1) / ncol(m), y=(y - 1) / nrow(m),
-  frame='Errors'
+  zz.raw$errors, x=(x - 1) / (ncol(m) - 1), y=(y - 1) / (nrow(m) - 1),
+  frame='Errors', stringsAsFactors=FALSE
 )
-o <- transform(ids_to_df(zz.raw$o, m), frame='Ids')
-
+o <- transform(
+  ids_to_df(zz.raw$o$val, m),
+  frame='Ids', .id=zz.raw$o$.id,
+  id=zz.raw$o$val, stringsAsFactors=FALSE
+)
+id.scalar <- match(o$.id, zz.vec$.id)
+o <- transform(
+  o, els=with(zz.vec[id.scalar,], c.rep * r.rep * ifelse(j == 'diag', 4L, 1L)),
+  j=zz.vec[id.scalar, 'j']
+)
+o <- transform(o, i=ave(.id, .id, FUN=seq_along))
+ptypes <- c('mid', 'end', 'end', rep('child', 5))
+o <- transform(o, ptype=ptypes[(i - 1) %/% els + 1])
 data <- list(o=o, errors=errors)
 
-frames <- 1:10
-for(i in frames) {
-  cat(sprintf("\rFrame %04d", i))
-  d <- lapply(data, function(x) subset(x, .id == i))
+frames <- 15:50
+library(ggplot2)
+for(k in frames) {
+  cat(sprintf("\rFrame %04d", k))
+  d <- lapply(data, function(x) subset(x, .id == k))
 
   p <- ggplot(mapping=aes(x, y)) +
-    geom_point(data=d$o) +
+    geom_point(data=d$o, aes(color=as.character(ptype))) +
     geom_point(data=d$errors, aes(size=val)) +
     facet_wrap(~frame) +
+    coord_cartesian(ylim=c(0,1), xlim=c(0,1)) +
+    thm.blnk +
     NULL
   ggsave(
-    filename=sprintf('~/Downloads/mesh-anim-5/img-%04d.png', i),
+    filename=sprintf('~/Downloads/mesh-anim-5/img-%04d.png', k),
     plot=p,
     width=width/dpi, height=height/dpi, units='in', device='png',
     dpi=dpi
