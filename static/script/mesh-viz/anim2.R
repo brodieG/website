568af6f6fba2b347fbec9e2318763621c3ae0bca
brodieG <brodieG@users.noreply.github.com>
2019-12-28 13:58:16 -0800

    cricles for all errs, but too much

--- static/script/mesh-viz/anim2.R
+++ static/script/mesh-viz/anim2.R
@@ -50,14 +50,14 @@ for(i in seq_along(zz.vec$.id)) {
 # * to color based on o.m, o.b, o.a?  Or maybe better start colored.  And use
 #   err.list for the circles and arrows.  Circles for all, arrows from 2nd+ to
 #   1st.
-# 
+#
 # One issue with this approach is that we don't get to see the hypotenuse
 # highlighted.  Maybe instead use o.a/o.b to draw the hypotenuse.  Colors are
 # there from the get-go, but maybe the outline of the hypotenuse is what shows
 # up.
 #
 # Circles show up when ids go into err.list.
-# 
+#
 # Urgh, we're probably not going to do this with the faster version.
 
 thm.blnk <- list(
@@ -79,6 +79,9 @@ errors <- transform(
   zz.raw$errors, x=(x - 1) / (ncol(m) - 1), y=(y - 1) / (nrow(m) - 1),
   frame='Errors', stringsAsFactors=FALSE
 )
+
+# Colored points
+
 o <- transform(
   ids_to_df(zz.raw$o$val, m),
   frame='Ids', .id=zz.raw$o$.id,
@@ -90,23 +93,67 @@ o <- transform(
   j=zz.vec[id.scalar, 'j']
 )
 o <- transform(o, i=ave(.id, .id, FUN=seq_along))
-ptypes <- c('mid', 'end', 'end', rep('child', 5))
+ptypes <- c('#8da0cb', '#66c2a5', '#66c2a5', rep('#fc8d62', 5))
 o <- transform(o, ptype=ptypes[(i - 1) %/% els + 1])
-data <- list(o=o, errors=errors)
+
+# Boundary around colored points + hypotenuse
+
+o.p <- with(zz.raw,
+  rbind(
+    cbind(o.m, ptype='mid'),
+    cbind(o.a, ptype='end'),
+    cbind(o.b, ptype='end')
+) )
+o.p <- cbind(o.p, ids_to_df(o.p$val, m))
+o.p[['frame']] <- 'Ids'
+
+o.a <- with(zz.raw$o.a, cbind(.id, ids_to_df(val, m)))
+o.b <- with(
+  zz.raw$o.b, cbind(.id, setNames(ids_to_df(val, m), c('xend','yend','zend')))
+)
+o.a <- transform(o.a, i=ave(.id, .id, FUN=seq_along))
+o.b <- transform(o.b, i=ave(.id, .id, FUN=seq_along))
+o.ab <- merge(o.a, o.b, by=c('.id', 'i'), all=TRUE)
+o.ab[['frame']] <- 'Ids'
+
+# Error circle / mappings
+
+err.ids <- lapply(zz.raw$err.ids, unlist)
+ids.len <- lengths(err.ids)
+err.cir <- cbind(
+  data.frame(.id=rep(zz.vec$.id, ids.len)),
+  ids_to_df(unlist(err.ids), m)
+)
+
+
+# arrows
+
+data <- list(
+  o=o, errors=errors, o.m=zz.raw$o.m,
+  o.p=o.p, o.ab=o.ab, err.cir=err.cir
+)
+
 
 frames <- 15:50
 library(ggplot2)
-for(k in frames) {
+# for(k in frames) {
+  k <- 60
   cat(sprintf("\rFrame %04d", k))
   d <- lapply(data, function(x) subset(x, .id == k))
 
   p <- ggplot(mapping=aes(x, y)) +
-    geom_point(data=d$o, aes(color=as.character(ptype))) +
+    geom_segment(data=d$o.ab, aes(xend=xend, yend=yend)) +
+    geom_point(
+      data=d$o, aes(color=I(ptype), fill=I(ptype)),
+    ) +
+    geom_point(data=d$o.p, shape=21L, fill=NA) +
     geom_point(data=d$errors, aes(size=val)) +
+    geom_point(data=d$err.cir, shape=21L, fill=NA, size=18) +
     facet_wrap(~frame) +
     coord_cartesian(ylim=c(0,1), xlim=c(0,1)) +
     thm.blnk +
     NULL
+  p
   ggsave(
     filename=sprintf('~/Downloads/mesh-anim-5/img-%04d.png', k),
     plot=p,
