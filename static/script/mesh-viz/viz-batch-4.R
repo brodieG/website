9b2ca227ede048ab5112f8a7cbf1a2cd506fd7fc
brodieG <brodieG@users.noreply.github.com>
2020-01-12 14:31:01 -0500

    tweaking water

--- static/script/mesh-viz/viz-batch-4.R
+++ static/script/mesh-viz/viz-batch-4.R
@@ -68,123 +68,27 @@ render_scenes(
 )
 stop('done')
 
-n <- 50
-seqv <- seq(-pi,pi, length.out=n + 1)
-df <- expand.grid(x=seqv, y=seqv)
-hz <- 1
-# df <- transform(
-#   df, z=
-#     1 * sin(2*x*hz + 4 * y*hz) +
-#     .8 * sin(2*x*hz - 3 * y*hz) +
-#     .4 * sin(10*x*hz + 6 * y* hz)
-# )
-t <- 0
-# df <- transform(
-#   df, z=
-#     .125 * sin(2*((x + pi)*hz)^2 + 4*(y*hz)^2) +
-#     .10 * sin(3*((x - pi)*hz)^2 + 2*((y + pi)*hz)^2) +
-#     .125 * sin(3*((x - 2 *pi)*hz)^2 + 6*((y + pi)*hz)^2)
-# )
-df <- transform(
-  df, z=
-    .2 * sin(2*(x + t)*hz + 4 * (y + t)*hz) +
-    .125 * sin(2*((x + t + pi)*hz)^2 + 4*((y + t)*hz)^2) +
-    .10 * sin(3*((x - pi + t)*hz)^2 + 2*((y + pi + t)*hz)^2) +
-    .125 * sin(3*((x - 2 *pi + t)*hz)^2 + 6*((y + pi + t)*hz)^2)
-)
-df <- transform(
-  df, z=
-    .5 * sin(2*(x+t)*hz + 4 * (y+t)*hz) +
-    .125 * sin(2*((x+t + 1.5*pi)*hz)^2 + 4*((y+t)*hz)^2) +
-    .125 * sin(3*((x+t - pi/2)*hz)^2 + 2*((y + t + 2*pi)*hz)^2) +
-    .3 * sin(2*(x+t)*hz - 3 * (y+t)*hz)
-  )
-df <- transform(
-  df, z=
-    .5 * sin(2*(x+t)*hz + 4 * (y+t)*hz) +
-    .125 * sin(2*(((x+t) + pi)*hz)^2 + 4*((y+t)*hz)^2) +
-    .125 * sin(3*(((x+t) + pi/4)*hz)^2 + 2*(((y+t) + pi)*hz)^2) +
-    .3 * sin(2*(x+t)*hz - 3 * (y+t)*hz)
-  )
-# df <- transform(df, z=sin(2*x + 4 * y))
-library(ggplot2)
-ggplot(df, aes(x=x, y=-y, fill=z)) + geom_raster() + scale_fill_viridis_c()
+source('static/script/mesh-viz/viz-waves.R')
+# f1 <- tempfile()
+# f2 <- tempfile()
+# f3 <- tempfile()
 
-# f <- tempfile()
-mesh <- shadow::mesh_tri(transform(df, y=z*.05, z=y), c(n, n)+1)
-m1 <- mesh
-m1[] <- lapply(mesh, '[', 1)
+water_surface(f1, 50)
+water_surface(f2, 50, t=pi/4)
+water_surface(f3, 50, t=pi/2)
 
-# Need to find all the vertices on the edges and generate a border out of them
-
-xu <- unlist(mesh[,'x'])
-zu <- unlist(mesh[,'z'])
-yu <- unlist(mesh[,'y'])
-
-x.lo <- min(xu)
-x.hi <- max(xu)
-z.lo <- min(zu)
-z.hi <- max(zu)
-y.lo <- -10
-if(y.lo > min(yu)) stop('need lower y')
-
-vertex_arrange <-function(a, b, y, lo) {
-  which.a <- b == lo
-  base.a <- a[which.a]
-  o <- order(base.a)
-  ao <- base.a[o]
-  d <- duplicated(ao)
-  aou <- ao[!d]
-  you <- y[which.a][o][!d]
-  list(aou, you)
-}
-zx.lo <- vertex_arrange(zu, xu, yu, x.lo)
-zx.hi <- vertex_arrange(zu, xu, yu, x.hi)
-xz.lo <- vertex_arrange(xu, zu, yu, z.lo)
-xz.hi <- vertex_arrange(xu, zu, yu, z.hi)
-
-to_mesh <- function(v, w, y, y.lo) {
-  lv <- length(v)
-  y.lo.r <- rep(y.lo, lv - 1)
-
-  L <- matrix(
-    list(
-      c(v[-lv], v[-1]), c(v[-lv], v[-1]), c(v[-1], v[-lv]),
-      c(y[-lv], y.lo.r), c(y.lo.r, y[-1]),  c(y[-1], y.lo.r),
-      rep(w, (lv - 1) * 2), rep(w, (lv - 1) * 2), rep(w, (lv - 1) * 2)
-    ),
-    3
-  )
-}
-mzx.lo <- to_mesh(zx.lo[[1]], x.lo, zx.lo[[2]], y.lo)[,3:1]
-mzx.hi <- to_mesh(zx.hi[[1]], x.hi, zx.hi[[2]], y.lo)[,3:1]
-mxz.lo <- to_mesh(xz.lo[[1]], z.lo, xz.lo[[2]], y.lo)
-mxz.hi <- to_mesh(xz.hi[[1]], z.hi, xz.hi[[2]], y.lo)
-# bot <- matrix(
-#   list(
-#     c(x.lo, x.lo), c(x.hi, x.hi), c(x.hi, x.lo),
-#     c(y.lo, y.lo), c(y.lo, y.lo), c(y.lo, y.lo),
-#     c(z.lo, z.lo), c(z.lo, z.hi), c(z.hi, z.hi)
-#   ), 3
-# )
-mesh2 <- mesh[,1:3]
-# mesh2[] <- Map(c, mesh2, mzx.lo, mzx.hi, mxz.lo, mxz.hi, bot)
-mesh2[] <- Map(c, mesh2, mzx.lo, mzx.hi, mxz.lo, mxz.hi)
-mesh3 <- mesh2
-
-# obj <- mesh_to_obj(mesh3, c(0, y.lo * .98, 0))
-obj <- mesh_to_obj(mesh3, c(0, -5, 0))
-# obj <- mesh_to_obj(mesh3)
-writeLines(obj, f)
 # par3d(windowRect=c(0, 0, 600, 600))
 # plot3d(readOBJ(f), color='grey')
 
 # mat.w <- dielectric(color='#F0F0FF', refraction=1.3)
 # mat.w <- dielectric(color='#E5E5FF', refraction=1.3)
 mat.w <- dielectric(color='#E0F5FF', refraction=1.3)
-objrr3 <- obj_model(f, material=mat.w, scale=rep(1.5/(2*pi), 3), y=.525)
-objrr2 <- obj_model(f, material=mat.w, scale=rep(1.5/(2*pi), 3), y=.4)
-objrr1 <- obj_model(f, material=mat.w, scale=rep(1.5/(2*pi), 3), y=.2)
+dy1 <- .2
+dy2 <- .4
+dy3 <- .525
+objrr3 <- obj_model(f3, material=mat.w, scale=rep(1.5/(2*pi), 3), y=dy3)
+objrr2 <- obj_model(f2, material=mat.w, scale=rep(1.5/(2*pi), 3), y=dy2)
+objrr1 <- obj_model(f1, material=mat.w, scale=rep(1.5/(2*pi), 3), y=dy1)
 
 mult <- 1.3
 light.narrow <- sphere(
@@ -214,55 +118,58 @@ scn.base.2 <- dplyr::bind_rows(
   ),
   NULL
 )
-dy2 <- .400
-dy1 <- .200
-dy3 <- .600
 dro <- .10
 dri <- .075
+dyy1 <- dy1 + 0
+dyy2 <- dy2 + 0
+dyy3 <- dy3 + 0
 # mat.d <- diffuse(color='#55CC55')
-mat.d <- diffuse(color='black')
+mat.d <- diffuse(color='grey10')
 scn.4a <- dplyr::bind_rows(
   scn.base.2, objrr1,
-  disk(radius=dro, inner_radius=dri, y=dy1, z=-.5, material=mat.d),
-  disk(radius=dro, inner_radius=dri, y=dy1, x=-.5, material=mat.d),
-  disk(radius=dro, inner_radius=dri, y=dy1, material=mat.d)
+  disk(radius=dro, inner_radius=dri, y=dyy1, z=-.5, material=mat.d),
+  disk(radius=dro, inner_radius=dri, y=dyy1, x=-.5, material=mat.d),
+  disk(radius=dro, inner_radius=dri, y=dyy1, material=mat.d)
 )
 scn.4b <- dplyr::bind_rows(
   scn.base.2, objrr2,
-  disk(radius=dro, inner_radius=dri, y=dy2, x=-.5, material=mat.d),
-  disk(radius=dro, inner_radius=dri, y=dy2, material=mat.d)
+  disk(radius=dro, inner_radius=dri, y=dyy2, x=-.5, material=mat.d),
+  disk(radius=dro, inner_radius=dri, y=dyy2, material=mat.d)
 )
 scn.4c <- dplyr::bind_rows(
   scn.base.2, objrr3,
-  disk(radius=dro, inner_radius=dri, y=dy3, x=-.5, material=mat.d),
+  disk(radius=dro, inner_radius=dri, y=dyy3, x=-.5, material=mat.d),
 )
 
 # rez <- 800
 # samp <- 300
-rez <- 300
-samp <- 150
-# rez <- 100
-# samp <- 25
+# rez <- 400
+# samp <- 500
+rez <- 200
+samp <- 25
 
-file <- next_file('~/Downloads/mesh-viz/small-mesh/simple-mesh-new2-')
 # scns <- list(scn.4a, scn.4b, scn.4c)
 # scns <- list(scn.4b)
+file <- next_file('~/Downloads/mesh-viz/small-mesh/simple-mesh-new2-')
 # render_scene(
 render_scenes(
   list(scn.4a, scn.4b, scn.4c),
+  # scn.4b,
+  #filename=file,
+  filename='~/Downloads/mesh-viz/small-mesh/water-new7c-%d.png',
   height=rez, width=rez, samples=samp,
-  lookfrom=c(0, 3, 1.5), lookat=c(0, 0, 0),
-  # lookfrom=c(0, 4, 1), lookat=c(0, 0, 0),
-  # fov=25,
-  fov=35,
+  # lookfrom=c(0, 3, 1.5), lookat=c(0, 0, 0),
+  lookfrom=c(0, 3, 1.5), 
+  lookat=c(0, .5, 0), fov=35,
+  # lookat=c(-.25, .5, 0), fov=15,
+  # lookfrom=c(2, 1, 2), lookat=c(0, 0, 0),
+  # fov=10,
   # fov=0, ortho_dimensions=c(1.75,1.75),
   aperture=0,
   camera_up=c(0,1,0),
   clamp=3,
   backgroundlow=bg, backgroundhigh=bg,
-  ambient_light=TRUE,
-  # filename=file
-  filename='~/Downloads/mesh-viz/small-mesh/water-new5-%d.png'
+  ambient_light=TRUE
 )
 
 rez <- 200
@@ -296,8 +203,8 @@ render_scene(
   height=rez, width=rez, samples=samp,
   lookfrom=c(0, 2, 7),
   # lookfrom=c(-7, 4, 0),
-  lookat=c(0, 0, 0),
-  fov=25,
+  # lookat=c(0, 0, 0), fov=25,
+  lookat=c(-.25, 0, 0), fov=25,
   aperture=0,
   camera_up=c(0,1,0),
   clamp=3,
