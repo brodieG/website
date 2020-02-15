9ffce3c25239fd9deca3e9ae16abbdd8476a615a
brodieG <brodieG@users.noreply.github.com>
2020-02-14 21:58:11 -0500

    trim png supports columns

--- static/script/mesh-viz/viz-lib.R
+++ static/script/mesh-viz/viz-lib.R
@@ -642,24 +642,75 @@ cbind_pngs <- function(input, output) {
   for(i in seq_along(colc)) d[,colcs[i]:colc[i],] <- pngs[[i]]
   png::writePNG(d, output)
 }
-# trim white or near white rows (for raster image rows are the x coordinatese of
-# the arrays)
+# trim white or near white rows/col (for raster image rows are the x
+# coordinatese of the arrays).  We don't bother with alpha as that will require
+# us cheking if it has the channel and different handling for it.
+#
+# @param pngs a list of png arrays (3D), with or without alpha
+# @param tol numeric(2), tolerance for rows and cols in that order
+# @param pad numeric(2), pixels of padding to add to result will just be full
+#   opacity white
+# @return list of png arrays
 
-trim_png_row <- function(pngs, tol=2) {
-  vetr(list(), INT.1 && all_bw(., 0, 255))
+trim_png <- function(pngs, tol=c(2,2), pad=c(0,0)) {
+  res <- trim_png_row(pngs, tol[1], pad[1])
+  trim_png_col(res, tol[2], pad[2])
+}
+
+trim_png_internal <- function(pngs, tol=2, pad=0, which) {
+  vetr(
+    list() && length(.) > 0, INT.1 && all_bw(., 0, 255), INT.1.POS,
+    which=isTRUE(. %in% c('row', 'col'))
+  )
+  if(which == 'row') {
+    sumf <- rowSums
+    readf <- function(x, idx) x[idx,,]
+    writef <- function(x, idx, val) {
+      x[idx,,] <- val
+      x
+    }
+    tar.dim <- 1
+    dim.len <- dim(pngs[[1]])[1]
+  } else {
+    sumf <- colSums
+    readf <- function(x, idx) x[,idx,]
+    writef <- function(x, idx, val) {
+      x[,idx,] <- val
+      x
+    }
+    tar.dim <- 2
+    dim.len <- dim(pngs[[1]])[2]
+  }
   tol <- tol / 255
   dims <- sapply(pngs, dim)
   if(!is.matrix(dims)) stop("unequal dims")
   if(length(unique(dims[1,])) > 1 || length(unique(dims[2,])) > 1)
     stop("unequal dim2")
 
-  pngsum <- rowSums(sapply(pngs, function(x) rowSums(x < 1 - tol))) == 0
+  pngsum <- rowSums(sapply(pngs, function(x) sumf(x < 1 - tol))) == 0
   a <- min(which(!pngsum))
   b <- min(which(!rev(pngsum)))
 
   a.not <- seq_len(a - 1L)
-  b.not <- seq_len(b - 1L) + length(pngsum) - b + 1
+  b.not <- seq_len(b - 1L) + dim.len - b + 1
 
-  lapply(pngs, function(x) x[-c(a.not, b.not),,])
+  tmp <- lapply(pngs, readf, -c(a.not, b.not))
+  dim.tmp <- dim(tmp[[1]])
+  if(pad) {
+    dim.pad <- dim.tmp
+    dim.pad[tar.dim] <- dim.pad[tar.dim] + 2*pad
+    res.tpl <- array(1, dim.pad)
+    lapply(tmp, writef, idx=seq_len(dim.tmp[tar.dim]) + pad, x=res.tpl)
+  } else {
+    tmp
+  }
 }
+trim_png_row <- function(pngs, tol=2, pad=0) {
+  trim_png_internal(pngs, tol, pad, which='row')
+}
+trim_png_col <- function(pngs, tol=2, pad=0) {
+  trim_png_internal(pngs, tol, pad, which='col')
+}
+
+
 
