1993de6c4371440d0b5c48d6dd71320eea817a09
brodieG <brodieG@users.noreply.github.com>
2020-04-22 20:35:59 -0400

    add hula and initial rayrender views

--- static/post/2020-02-13-on-nse_files/scripts/svg-import.R
+++ static/post/2020-02-13-on-nse_files/scripts/svg-import.R
@@ -107,5 +107,105 @@ iny <- unlist(lapply(inside, '[[', 2))
 allx <- c(outx, inx)
 ally <- -c(outy, iny) + 1
 
-ind <- decido::earcut(cbind(allx, ally), holes=length(outx) + 1)
-decido::plot_ears(cbind(allx, ally), ind, col = "#1e64b6")
+# - Hula -----------------------------------------------------------------------
+
+# This one is all beziers
+
+hbrle <- with(dh, rle(cmd == "C" | cmd == ""))
+hbends <- with(hbrle, cumsum(lengths)[values])
+hbstarts <- with(hbrle, cumsum(lengths)[!values])
+
+hbzs <- Map(
+  function(start, end) {
+    points <- dh[start:end, c('x', 'y')]
+    BezierGrob(points[[1]], points[[2]], stepFn=nSteps(20))
+  },
+  hbstarts,
+  hbends
+)
+hbzsp <- lapply(hbzs, BezierPoints)
+hbzsp <- lapply(
+  hbzsp, function(z) list(z[[1]] / par()$pin[[1]], z[[2]] / par()$pin[[2]])
+)
+# plot.new()
+# grid.draw(grid.lines(hbzsp[[1]][[1]], hbzsp[[1]][[2]]))
+# grid.draw(grid.lines(hbzsp[[2]][[1]], hbzsp[[2]][[2]]))
+
+hulax <- c(hbzsp[[1]][[1]], hbzsp[[2]][[1]])
+hulay <- -c(hbzsp[[1]][[2]], hbzsp[[2]][[2]]) + 1
+
+# hind <- decido::earcut(cbind(hulax, hulay), holes=length(hbzsp[[1]][[1]]) + 1)
+# decido::plot_ears(cbind(hulax, hulay), hind, col = "#1e64b6")
+#
+# Why do we need to run this twice!!?  Maybe because device is not init?
+
+# - Plotting -------------------------------------------------------------------
+
+# ind <- decido::earcut(cbind(allx, ally), holes=length(outx) + 1)
+# decido::plot_ears(cbind(allx, ally), ind, col = "#1e64b6")
+
+ind <- decido::earcut(cbind(outx, 1-outy))
+decido::plot_ears(cbind(outx, 1-outy), ind, col = "#1e64b6")
+
+stop()
+library(rayrender)
+
+epr <- extruded_polygon(
+  # xy.coords(allx, ally),
+  # holes=length(outx),  # for some reason index off by 1 relative to decido
+  xy.coords(outx, 1-outy),
+  top=0, bottom=-.2,
+  material=diffuse(color="#1e64b6"),
+  plane='xy',
+  angle=c(0, 180, -90), order_rotation=c(3, 2, 1)
+)
+eph <- extruded_polygon(
+  xy.coords(hulax, hulay),
+  holes=length(hbzsp[[1]][[1]]),
+  top=0.05, bottom=-0.05,
+  material=diffuse(color="gray"),
+  plane='xy',
+  angle=c(0, 180, -90), order_rotation=c(3, 2, 1)
+)
+l <- sphere(x=4, y=4, z=-4, radius=.25, material=light(intensity=20* 4))
+f <- xz_rect(y=0, xwidth=4, zwidth=24, material=diffuse(color='white'))
+b <- xy_rect(
+  z=-3, ywidth=10, xwidth=10, material=diffuse(color='white')
+)
+scene <- dplyr::bind_rows(
+  epr,
+  # cube(x=-.5, material=diffuse(color="#1e64b6")),
+  # eph,
+  # l,
+  # f, b
+)
+w <- h <- 100
+s <- 100
+bg <- '#555555'
+render_scene(
+  scene, width=w, height=h, samples=s,
+  # lookfrom=c(0, 10, 0.000001), fov=10,
+  fov=7, lookfrom=c(.5, 3, 10),
+  lookat=c(0.5, 0.40, 0),
+  ambient_light=TRUE,
+  filename=sprintf('~/Downloads/rlogo/a-%d.png', sample(1e4:9e4, 1))
+)
+
+stop('boom')
+
+w <- h <- 200
+s <- 50
+bg <- '#555555'
+
+library(rayrender)
+mat <- diffuse(color='#CCCCCC')
+xy <- xy.coords(c(-1, -1, 1, 1)/2, c(-1, 1, 1, -1)/2)
+scene1 <- dplyr::bind_rows(
+  cube(x=-.75, material=mat),
+  extruded_polygon(xy, top=.5, bottom=-.5, x=.75, material=mat)
+)
+render_scene(
+  scene1, width=400, height=200, samples=200,
+  lookfrom=c(0, 3, 10), fov=10
+)
+
