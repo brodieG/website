fa2103c5732855a464e5c2e7caa996f9196a03af
brodieG <brodieG@users.noreply.github.com>
2020-05-05 09:09:43 -0400

    static assets

--- static/post/2020-02-13-on-nse_files/scripts/svg-import.R
+++ static/post/2020-02-13-on-nse_files/scripts/svg-import.R
@@ -1,3 +1,4 @@
+# source('static/post/2020-02-13-on-nse_files/scripts/svg-import.R')
 # - Basic Processing -----------------------------------------------------------
 
 # Logo stuff, very specific to this file and the SVG paths it contains, don't
@@ -27,6 +28,18 @@ dr[[2]] <- dr[[2]] / width
 dr[[3]] <- dr[[3]] / height
 dh[[2]] <- dh[[2]] / width
 dh[[3]] <- dh[[3]] / height
+
+Absolute coordinates in caps
+
+# M: moveto
+# L: lineto
+# Z: closepath
+# ZM: cloepath/moveto?
+# C: curveto - cubic bezier, which start at the previous point, list the two
+#    control point, and then list the end point, so four coordinates, of which
+#    one is the last coordinate of the prior command.
+# 
+# Pull out the bezier segments
 
 # - R Path ---------------------------------------------------------------------
 
@@ -114,8 +127,9 @@ hbzs <- Map(
   hbends
 )
 hbzsp <- lapply(hbzs, BezierPoints)
+hbzspi <- lapply(hbzsp, lapply, unit, 'inches')
 hbzsp <- lapply(
-  hbzsp, function(z) list(z[[1]] / par()$pin[[1]], z[[2]] / par()$pin[[2]])
+  hbzspi, function(x) list(convertX(x[[1]], 'npc'),convertY(x[[2]], 'npc'))
 )
 # plot.new()
 # grid.draw(grid.lines(hbzsp[[1]][[1]], hbzsp[[1]][[2]]))
@@ -126,76 +140,13 @@ hulay <- -c(hbzsp[[1]][[2]], hbzsp[[2]][[2]]) + 1
 
 # hind <- decido::earcut(cbind(hulax, hulay), holes=length(hbzsp[[1]][[1]]) + 1)
 # decido::plot_ears(cbind(hulax, hulay), hind, col = "#1e64b6")
-#
-# Why do we need to run this twice!!?  Maybe because device is not init?
 
 # - Plotting -------------------------------------------------------------------
 
 # ind <- decido::earcut(cbind(allx, ally), holes=length(outx) + 1)
 # decido::plot_ears(cbind(allx, ally), ind, col = "#1e64b6")
 
-ind <- decido::earcut(cbind(outx, 1-outy))
-decido::plot_ears(cbind(outx, 1-outy), ind, col = "#1e64b6")
+# ind <- decido::earcut(cbind(outx, 1-outy))
+# decido::plot_ears(cbind(outx, 1-outy), ind, col = "#1e64b6")
 
-stop()
-library(rayrender)
-
-epr <- extruded_polygon(
-  xy.coords(allx, ally),
-  holes=length(outx),  # for some reason index off by 1 relative to decido
-  # xy.coords(outx, 1-outy),
-  top=0, bottom=-.2,
-  material=diffuse(color="#1e64b6"),
-  plane='xy',
-  angle=c(0, 180, -90), order_rotation=c(3, 2, 1)
-)
-eph <- extruded_polygon(
-  xy.coords(hulax, hulay),
-  holes=length(hbzsp[[1]][[1]]),
-  top=0.05, bottom=-0.05,
-  material=diffuse(color="gray"),
-  plane='xy',
-  angle=c(0, 180, -90), order_rotation=c(3, 2, 1)
-)
-l <- sphere(x=4, y=4, z=-4, radius=.25, material=light(intensity=20* 4))
-f <- xz_rect(y=0, xwidth=4, zwidth=24, material=diffuse(color='white'))
-b <- xy_rect(
-  z=-3, ywidth=10, xwidth=10, material=diffuse(color='white')
-)
-scene <- dplyr::bind_rows(
-  epr,
-  # cube(x=-.5, material=diffuse(color="#1e64b6")),
-  eph,
-  # l,
-  # f, b
-)
-w <- h <- 100
-s <- 100
-bg <- '#555555'
-render_scene(
-  scene, width=w, height=h, samples=s,
-  # lookfrom=c(0, 10, 0.000001), fov=10,
-  fov=7, lookfrom=c(.5, 3, 10),
-  lookat=c(0.5, 0.40, 0),
-  ambient_light=TRUE,
-  filename=sprintf('~/Downloads/rlogo/a-%d.png', sample(1e4:9e4, 1))
-)
-
-stop('boom')
-
-w <- h <- 200
-s <- 50
-bg <- '#555555'
-
-library(rayrender)
-mat <- diffuse(color='#CCCCCC')
-xy <- xy.coords(c(-1, -1, 1, 1)/2, c(-1, 1, 1, -1)/2)
-scene1 <- dplyr::bind_rows(
-  cube(x=-.75, material=mat),
-  extruded_polygon(xy, top=.5, bottom=-.5, x=.75, material=mat)
-)
-render_scene(
-  scene1, width=400, height=200, samples=200,
-  lookfrom=c(0, 3, 10), fov=10
-)
 
