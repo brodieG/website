75b655074e0c1fda5b7b1969ec2ccc3c95dd3c84
brodieG <brodieG@users.noreply.github.com>
2020-06-20 09:52:18 -0400

    road satisfactory

--- static/post/2020-02-17-quosures_files/script/portal.R
+++ static/post/2020-02-17-quosures_files/script/portal.R
@@ -8,6 +8,7 @@ library(rayrender)
 gold_mat <- microfacet(
   roughness=0.1, eta=c(0.216,0.42833,1.3184), kappa=c(3.239,2.4599,1.8661)
 )
+gold <- "#BB5500"
 zz <- parse_paths(url)
 ww <- interp_paths(zz, normalize=TRUE)
 paths <- lapply(
@@ -66,7 +67,7 @@ comp_inside <- function(
     lapply(
       seq_len(ncol(vecs.n) - 1L),
       function(i) {
-        if(i == light_index) material=light(intensity=10)
+        if(i == light_index) material=light(intensity=5)
         list(
           triangle(
             v1=hex.in[,i], v2=hex.out[,i], v3=hex.out[,i+1],
@@ -101,18 +102,18 @@ attr(hex, 'starts') <- 8
 # Should have done everything directly in x-y plane...
 
 h2 <- hex[1:7,]
-brick.depth <- 3
-depth <- 5
+brick.depth <- 10
+depth <- 20
 obs <- c(0, 1, 0)
 bag <- comp_inside(
-  h2, 0, obs, depth=depth, diffuse(color='grey5'), light_index=6
+  h2, 0, obs, depth=depth, diffuse(color='#010102'), light_index=6
 )
 objs <- dplyr::bind_rows(
   lapply(
     paths[-c(1, length(paths))],
-    extrude_path, material=gold_mat, top=.1
+    extrude_path, material=gold_mat, top=.025
   ),
-  extrude_path(hex, material=diffuse('gray30'), top=.1),
+  extrude_path(hex, material=diffuse('gray10'), top=.025),
   bag
 )
 n <- 10
@@ -138,14 +139,17 @@ b.p.obj <- dplyr::bind_rows(
     seq_along(brick.dots[[1]]),
     function(i)
       sphere(
-        radius=.025, x=brick.dots[[1]][i], y=brick.dots[[2]][i],
-        material=gold_mat
+        radius=.01, x=brick.dots[[1]][i], y=brick.dots[[2]][i],
+        material=diffuse(color='blue')
 ) ) )
 # actual projected version
 # https://en.wikipedia.org/wiki/Line%E2%80%93plane_intersection
 
 brick.path.2 <- brick.path
+brick.path.2[2:3, 1] <- c(.16)
+brick.path.2[2:3, 2] <- c(.44)
 brick.path.2[2:3, 1] <- c(.18)
+brick.path.2[2:3, 2] <- c(.47)
 
 p0 <- c(0, 0, 0)
 p1 <- c(0, y.rise, -brick.depth)
@@ -189,25 +193,51 @@ int.test <- dplyr::bind_rows(
 
 # Interpolate and rotate back
 
-int.dots <- bezier_interp_even(list(x=int.rot[,1], y=int.rot[,3]), 100)
+n <- 400
+int.dots <- bezier_interp_even(list(x=int.rot[,1], y=int.rot[,3]), n)
 int.dots.3d <- rx %*% rbind(int.dots[[1]], 0, int.dots[[2]])
 
 # Place each rung in between the points, compute angle from vector
 
 int.diff <- int.dots.3d[,-1] - int.dots.3d[,-ncol(int.dots.3d)]
 int.diff.n <- int.diff / rep(sqrt(colSums(int.diff ^ 2)), each=3)
+int.mid <- int.dots.3d[,-ncol(int.dots.3d)] + int.diff/2
 
+int.test <- dplyr::bind_rows(
+  lapply(
+    seq_len(ncol(int)),
+    function(i)
+      sphere(
+        x=int[1,i], y=int[2,i], z=int[3,i],
+        radius=.4, material=diffuse(color='green')
+      )
+  )
+)
+set.seed(1)
+x.off <- rnorm(n) * .025
+int.diff.obj <- dplyr::bind_rows(
+  lapply(
+    seq_len(ncol(int.diff)),
+    function(i)
+      cylinder(
+        x=int.mid[1,i] + x.off[i], y=int.mid[2,i], z=int.mid[3,i],
+        radius=.025, material=diffuse(color=gold),
+        # angle=c(0, -30, 90),
+        angle=c(0, -asin(int.diff.n[1,i])/pi*180, 90),
+        order_rotation=c(3, 2, 1)
+      )
+  )
+)
 int.obj <- dplyr::bind_rows(
   lapply(
     seq_len(ncol(int.dots.3d)),
     function(i)
       sphere(
         x=int.dots.3d[1,i], y=int.dots.3d[2,i], z=int.dots.3d[3,i],
-        radius=.1, material=gold_mat
+        radius=.1, material=diffuse(color='red')
       )
   )
 )
-
 bpp <- within(
   brick.path, {
     z <- -y / tan((angle)/180 * pi)
@@ -242,22 +272,24 @@ bg <- '#FFFFFF'
 render_scene(
   dplyr::bind_rows(
     group_objects(objs, group_angle=c(-90,0,0), group_translate=c(0,.5,0)),
-    # bricks,
-    b.p.obj,
-    int.obj,
-    int.test,
-    sphere(z=5, y=2, x=5, radius=3, material=light(intensity=5)),
-    sphere(radius=10, material=diffuse(), flipped=TRUE),
+    # b.p.obj,
+    # int.obj,
+    int.diff.obj,
+    # int.test,
+    sphere(z=15, y=6, x=15, radius=6, material=light(intensity=3)),
+    sphere(z=-15, y=6, x=-15, radius=6, material=light(intensity=10)),
+    sphere(radius=36, material=diffuse(), flipped=TRUE),
+    sphere(z=-15, y=6, x=15, radius=6, material=light(intensity=10)),
   ),
   filename=next_file("~/Downloads/rlang/imgs/img-"),
   lookfrom=c(0, .5, 1), lookat=c(0, .5, 0),
-  # lookfrom=c(2, .5, 1), lookat=c(0, .5, 0),
+  # lookfrom=c(0, 12, -1), lookat=c(-3, .5, -5),
   # lookfrom=c(0, .5, 5), lookat=c(0, .5, 0),
-  # width=800, height=800,
-  samples=5,
+  # width=600, height=600, samples=100,
+  samples=100,
   clamp_value=5,
-  # fov=60,
-  fov=90,
+  fov=60,
+  # fov=90,
   aperture=0
 )
 # objs <- dplyr::bind_rows(
