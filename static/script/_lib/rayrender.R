8cadf778a90cb00470c99c4157b9248042bddc0e
brodieG <brodieG@users.noreply.github.com>
2020-11-25 14:11:25 -0500

    fix next_file

--- static/script/_lib/rayrender.R
+++ static/script/_lib/rayrender.R
@@ -7,15 +7,18 @@
 
 library(vetr)
 next_file <- function(x) {
-  pat <-  "(\\d+)\\.png$"
-  vetr(character(1L) && grepl(., pat))
+  vetr(character(1L) && grepl("(.*?)(\\d+)\\.png$", .))
+  pat <- "(.*?)(\\d+)\\.png$"
   dir <- dirname(x)
   f <- basename(x)
-  fl <- list.files(dir, full.names=TRUE, pattern=pat)
-  root <- sub(pat, "", x)
+  matches <- regmatches(f, regexec(pat, f))
+  root <- matches[[1]][2]
+  fl <- list.files(dir, pattern=paste0("^", root, "(\\d+)\\.png$"))
   fnum <-
     if(!length(fl)) 0L
-    else max(as.integer(sub(".*?(\\d+)\\.png", "\\1", fl, perl=TRUE)), 0L)
+    else max(
+      as.integer(vapply(regmatches(fl, regexec(pat, fl)), '[', "", 3))
+    )
 
   vet(INT.1.POS, fnum, stop=TRUE)
   file.path(dir, paste0(root, sprintf("%03d", fnum + 1), ".png", collapse=""))
