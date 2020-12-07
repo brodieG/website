1591744b1b0e5dc9d95ad8e06796dd1e1e7651a7
brodieG <brodieG@users.noreply.github.com>
2020-12-07 06:42:17 -0500

    easing fun 1st pass

--- static/script/_lib/plot.R
+++ static/script/_lib/plot.R
@@ -28,3 +28,57 @@ gtable_dim <- function(gtable, din=par()[['din']]) {
   )
   c(wknown + null.size * wnull, hknown + null.size * hnull)
 }
+
+# Produce an Easing Path
+#
+# Given two functions will interpolate `n` points using `fin` for `[0,
+# domain[1]]` and `fout` for `[1 - domain[2], 1]`.  If sum `domain` is less than
+# one, then the interpolation will be filled in with a Hermite spline matching
+# the end and beginning slopes of the functions.
+#
+# @param n numeric how many points to output.
+# @param fin a function that accepts monotonically increasing inputs in [0-1]
+#    and maps them into monotonically increasing output in [0-1].
+# @param fout like fin, except that it's output will be used reversed to define
+#   the "easing out" part of the result.
+# @param domain numeric(2) how much of the output domain should be allocated
+#    respectively to points derived from `fin` and `fout`.  The sum of the two
+#    values should be less than or equal to one, with each value greater than
+#    or equal to zero.  If the sum of the two is less than one, the balance of
+#    the domain will be used to join the `fin` and `fout` portion of the outputs
+#    matching the slopes of the two with a Hermite spline.
+# @value numeric `n` values in [0-1] spaced according to the inputs.
+
+ease_in_out <- function(n, fin, fout, domain=c(.5, .5)) {
+  vetr(
+    INT.1.POS.STR, is.function(.), is.function(.),
+    numeric(2) && all_bw(., 0, 1) && sum(.) <= 1
+  )
+  mid <- 1 - sum(domain)
+  nin <- round(n * domain[1])
+  nout <- round(n * domain[2])
+  nmid <- n - nin - nout
+
+  if((nin < 2 || nout < 2) && nmid > 0) stop("Insufficient points")
+
+  pinxin <- seq(0, 1, length.out=nin)
+  pinx <- pinxin * domain[1]
+  pin <- fin(pinxin) * domain[1]
+
+  poutxin <- rev(seq(0, 1, length.out=nout))
+  poutx <- 1 - poutxin * domain[2]
+  pout <- 1 - fout(poutxin) * domain[2]
+
+  pmid <- if(nmid) {
+    mxin <- c(pinx[length(pinx)], poutx[1])
+    myin <- c(pin[length(pin)], pout[1])
+    slope1 <- diff(pin[length(pin) - 1:0]) / diff(pinx[length(pinx) - 1:0])
+    slope2 <- diff(pout[1:2]) / diff(poutx[1:2])
+
+    mx <- seq(pinx[length(pinx)], poutx[1], length.out=nmid)
+    my <- splinefunH(mxin, myin, c(slope1, slope2))(mx)
+    list(x=mx, y=my)
+  } else list(x=numeric(), y=numeric())
+
+  list(x=c(pinx, pmid[['x']], poutx), y=c(pin, pmid[['y']], pout))
+}
